0d9c808353b0b7bcda6b4f7a301e306cde363faf
Zenith <zenithyap88@gmail.com>
2023-01-10 21:14:19 +0800

    revert to local storage for deployment

--- project/src/pages/About.js
+++ project/src/pages/About.js
@@ -1,11 +1,18 @@
 import './text.css'
 import NavigationBar from './NavigationBar';
+import { Paper } from '@mui/material';
 
 export default function About() {
-    return(
-        <div className='bg'>
-            <NavigationBar/>
+  return (
+    <div>
+      <div className='bg'>
+        <NavigationBar />
         <div className='title'>About Me</div>
+      </div>
+      <h1>
+        test
+      </h1>
     </div>
-    )
+
+  )
 }
--- project/src/pages/Record.js
+++ project/src/pages/Record.js
@@ -1,101 +1,125 @@
 import NavigationBar from "./NavigationBar";
 import './text.css'
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import moment from 'moment'
 import Axios from 'axios'
-import axios from "axios";
 
-function EntryList({ list, deleteEntry }) {
-  return (
-    <div className='entry-list'>
-      {
-        list && list.map((item, i) => {
-          console.log(item.id)
-          return (
-            <div className='card mb-2 border-dark' key={i}>
-              <div className='card-body bg-dark text-white'>
-                <h4 className='card-title'>{item.date}</h4>
-                <p className='card-text>'>{item.text}</p>
-                <button className='btn btn-danger btn-sm' onClick={() => deleteEntry(item.id)}>Delete</button>
-              </div>
-            </div>
-          )
-        })
-      }
-    </div>
-  )
+function useRecord() {
+    const [entries, setEntries] = useState([])
+
+    const getEntriesFromStorage = () => JSON.parse(
+        window.localStorage.getItem('recordEntries')
+    )
+
+    // need to stringify item before parsing and storing to local storage
+    // React Hook useEffect contains a call to 'setEntries'. 
+    // Without a list of dependencies, this can lead to an infinite 
+    // chain of updates. To fix this, pass [] as a second argument to 
+    // the useEffect Hook
+    const setEntriesToStorage = items => 
+        window.localStorage.setItem('recordEntries', JSON.stringify(items))
+        useEffect(() => {
+            const entriesFromStorage = getEntriesFromStorage()
+            if (entriesFromStorage) {
+                setEntries(entriesFromStorage)
+            }
+        }, [])
+    
+    const storeEntry = entry => {
+        const newEntries = [entry, ...entries]
+        setEntries(newEntries)
+        setEntriesToStorage(newEntries)
+    }
+
+    const deleteEntry = index => {
+        const newEntries = [...entries.slice(0, index), ...entries.slice(index+1)]
+        setEntries(newEntries)
+        setEntriesToStorage(newEntries)
+    }
+
+    return [entries, storeEntry, deleteEntry]
+}
+
+function EntryList({list, deleteEntry}) {
+    // why got => e ?
+    // Higher order delete entry click handler
+    const handleDeleteEntry = index => e => {
+        deleteEntry(index)
+    }
+
+    return (
+        <div className='entry-list'>
+            {
+                list && list.map((item, i) => {
+                    const itemDate = moment(item.date).format('L')
+                    return (
+                        <div className='card mb-2 border-dark' key={i}>
+                            <div className='card-body bg-dark text-white'>
+                                <h4 className='card-title'>{itemDate}</h4>
+                                <p className='card-text>'>{item.message}</p>
+                                <button className='btn btn-danger btn-sm' onClick={handleDeleteEntry(i)}>Delete</button>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
 }
 
 // useRef.current.focus() purpose?
-function Entry({ addEntry, getRecord }) {
-  const [message, setMessage] = useState("")
-  const handleOnChange = e => setMessage(e.target.value)
-  const handleOnSubmit = e => {
-    e.preventDefault()
-    if (message && message.trim().length > 0) {
-      addEntry(message)
+function Entry({addEntry}) {
+    const [message, setMessage] = useState("")
+    const handleOnChange = e => setMessage(e.target.value)
+    const handleOnSubmit = e => {
+        e.preventDefault()
+        if (message && message.trim().length > 0) {
+            addEntry({
+                message,
+                date: Date.now()
+            })
+            Axios.post('http://localhost:3001/create', {
+                date: Date.now(),
+                text: message
+            }).then(() => {
+                console.log("Success")
+            })
+        }
+        setMessage("")
     }
-    setMessage("")
-  }
 
-  return (
-    <form onSubmit={handleOnSubmit}>
-      <div className="form-group">
-        <textarea
-          className="form-control mb-2"
-          value={message}
-          onChange={handleOnChange}
-          type="text"
-        />
-        <button disabled={message.trim().length === 0}
-          className="btn mb-2 btn-success form-control" type="submit">
-          Submit
-        </button>
-        <button
-          className="btn mb-2 btn-success form-control" onClick={getRecord}>
-          Show Record
-        </button>
-      </div>
-    </form>
-  )
+    return (
+        <form onSubmit={handleOnSubmit}>
+            <div className="form-group">
+                <textarea
+                    className="form-control mb-2"
+                    value={message}
+                    onChange={handleOnChange}
+                    type="text"
+                />
+                <button disabled={message.trim().length === 0}
+                    className="btn mb-2 btn-success form-control" type="submit">
+                        Submit
+                </button>
+            </div>
+        </form>
+    )
 }
 
 export default function Record() {
-  const getRecord = () => {
-    axios.get('http://localhost:3001/record').then((response) => {
-      setRecordArray(response.data)
-    })
-  }
-
-  const handleStoreEntry = (entry) => {
-    Axios.post('http://localhost:3001/create', {
-      date: moment(Date.now()).format('L'),
-      text: entry
-    }).then(() => {
-      console.log("Success")
-      getRecord()
-    })
-  }
-
-  const handleDeleteEntry = (index) => {
-    Axios.delete(`http://localhost:3001/delete/${index}`
-    ).then(() => {
-      getRecord()
-    })
-  }
-
-  const [recordArray, setRecordArray] = useState([]);
-
-  return (
-    <div className="bg">
-      <NavigationBar />
-      <div className="container">
-        <h1 className='title'>
-          Record
-        </h1>
-        <Entry addEntry={handleStoreEntry} getRecord={getRecord} />
-        <EntryList list={recordArray} deleteEntry={handleDeleteEntry} />
-      </div>
-    </div>
-  )
+    const [entries, storeEntry, deleteEntry] = useRecord();
+    const handleStoreEntry = (entry) => storeEntry(entry)
+    const handleDeleteEntry = (index) => deleteEntry(index)
+    return (
+        <div className="bg">
+            <NavigationBar/>
+            <div className="container">
+                <h1 className='title'>
+                    Record
+                </h1>
+                <Entry addEntry={handleStoreEntry}/>
+                <EntryList list={entries} deleteEntry={handleDeleteEntry}/>
+            </div>
+        </div>
+    )
 }
